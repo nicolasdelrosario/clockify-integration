ef219891fb20fbe633bb1384acf060d439bafb51
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-01-06 17:19:30 -0500

    feat: :sparkles: proceed with report
    this allow us to send the report to supabase or not.

---
+++ src/prompts/proceedReport.js
@@ -0,0 +1,15 @@
+import inquirer from 'inquirer'
+
+export const proceedReport = async () => {
+  const { proceed } = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'proceed',
+      message:
+        '¿Desea registrar las horas trabajadas y el pago de la semana de los usuarios?',
+      default: true,
+    },
+  ])
+
+  return proceed
+}
