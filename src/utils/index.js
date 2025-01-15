83b6362f83a288ee5dd6032ef0e64478dee877db
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-01-15 11:29:56 -0500

    chore: :technologist: exportable file

---
+++ src/utils/index.js
@@ -0,0 +1,3 @@
+export { calculateTotalHours } from './calculateTotalHours.js'
+export { calculateTotalPayment } from './calculateTotalPayment.js'
+export { getStartAndEndOfLastWeek } from './getStartAndEndOfLastWeek.js'
