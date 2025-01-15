2aba5fdf8a2d50590618c274ec2b4e240e933e92
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-01-15 11:28:31 -0500

    feat: :technologist: exportable file

--- src/email/index.js
+++ src/email/index.js
@@ -1,24 +1,2 @@
-import { Resend } from 'resend'
-import { env } from '../config/env.js'
-
-const { resendApiKey, letyMindEmail, emailRecipients } = env
-
-const resend = new Resend(resendApiKey)
-
-export default async function sendEmail(htmlContent) {
-  const recipients = emailRecipients.split(',').map(email => email.trim())
-
-  if (!recipients || recipients.length === 0)
-    return console.error('No hay destinatarios.')
-
-  const { error } = await resend.emails.send({
-    from: letyMindEmail,
-    to: recipients,
-    subject: 'Seguimiento semanal de uso de horas.',
-    html: htmlContent,
-  })
-
-  if (error) return console.error({ error })
-
-  console.log('Correo enviado correctamente')
-}
+export { sendEmail } from './sendEmail.js'
+export { reportTemplate } from './reportTemplate.js'
