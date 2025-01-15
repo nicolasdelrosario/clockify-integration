fab5aff487b7c18f33dac4eff67c4e3a0d446714
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-01-15 11:28:53 -0500

    feat: :technologist: exportable file

---
+++ src/services/supabase/index.js
@@ -0,0 +1,4 @@
+export { getWorkspace, createWorkspace } from './workspace.js'
+export { getUser, createUser } from './user.js'
+export { getPaymentPerHour } from './levels.js'
+export { registerWeeklyPayment } from './payment.js'
