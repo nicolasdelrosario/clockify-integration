12050941f0414d414ecadf1762cba3e553297d66
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-01-22 01:26:50 -0500

    feat: :sparkles: add proceed weekly report
    in order to know if generate a weekly report or not

---
+++ src/prompts/proceedWeeklyReport.js
@@ -0,0 +1,14 @@
+import inquirer from 'inquirer'
+
+export const proceedWeeklyReport = async () => {
+  const { proceed } = await inquirer.prompt([
+    {
+      type: 'confirm',
+      name: 'proceed',
+      message: '¿Desea generar un reporte semanal?',
+      default: true,
+    },
+  ])
+
+  return proceed
+}
