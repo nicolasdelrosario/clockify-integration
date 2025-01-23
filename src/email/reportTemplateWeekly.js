f2f2dac2837ad3283ed4f1d8b6200fb6a5c85173
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-01-23 12:54:21 -0500

    feat: :sparkles: new weekly template

---
+++ src/email/reportTemplateWeekly.js
@@ -0,0 +1,45 @@
+import { reportStyles } from './reportStyles.js'
+
+export const reportTemplateWeekly = report => {
+  const rows = report
+    .map(
+      entry => `
+      <tr class="table__row">
+        <td>${entry.company}</td>
+        <td>${entry.talent}</td>
+        <td>${entry.total_hours}</td>
+        <td>${entry.start_date}</td>
+        <td>${entry.end_date}</td>
+      </tr>
+    `
+    )
+    .join('')
+
+  return `
+    <html>
+      <head>
+        <style>
+          ${reportStyles}
+        </style>
+      </head>
+      <body>
+        <h1 class="title">Reporte Semanal</h1>
+        <p class="subtitle">Información de las horas trabajadas:</p>
+        <table class="table">
+          <thead class="table__head">
+            <tr class="table__row">
+              <th>Empresa</th>
+              <th>Talento</th>
+              <th>Horas Utilizadas</th>
+              <th>Fecha Inicio</th>
+              <th>Fecha Fin</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${rows}
+          </tbody>
+        </table>
+      </body>
+    </html>
+  `
+}
