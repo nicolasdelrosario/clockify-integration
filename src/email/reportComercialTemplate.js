388540711591606f9d5c3834f2b5ec77a26b0e31
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-01-23 12:53:49 -0500

    feat: :sparkles: new template for comercial team

---
+++ src/email/reportComercialTemplate.js
@@ -0,0 +1,47 @@
+import { reportStyles } from './reportStyles.js'
+
+export const reportComercialTemplate = report => {
+  const rows = report
+    .map(
+      entry => `
+      <tr class="table__row">
+        <td>${entry.company}</td>
+        <td>${entry.talent}</td>
+        <td>${entry.remaining_hours}</td>
+        <td>${entry.letycash.toFixed(2)}</td>
+        <td>${entry.start_date}</td>
+        <td>${entry.end_date}</td>
+      </tr>
+    `
+    )
+    .join('')
+
+  return `
+    <html>
+      <head>
+        <style>
+          ${reportStyles}
+        </style>
+      </head>
+      <body>
+        <h1 class="title">Reporte Mensual a Comercial</h1>
+        <p class="subtitle">Detalle de los pagos a realizar:</p>
+        <table class="table">
+          <thead class="table__head">
+            <tr class="table__row">
+              <th>Empresa</th>
+              <th>Talento</th>
+              <th>Horas Restantes</th>
+              <th>Letycash</th>
+              <th>Fecha Inicio</th>
+              <th>Fecha Fin</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${rows}
+          </tbody>
+        </table>
+      </body>
+    </html>
+  `
+}
