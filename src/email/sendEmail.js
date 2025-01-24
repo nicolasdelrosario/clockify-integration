ad232b61e2791a1c36b84e13f2131d5f59313f58
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-01-24 12:58:58 -0500

    feat: :sparkles: we can receive the email by prop

--- src/email/sendEmail.js
+++ src/email/sendEmail.js
@@ -5,16 +5,17 @@ const { resendApiKey, letyMindEmail, emailRecipients } = env
 
 const resend = new Resend(resendApiKey)
 
-export async function sendEmail(data) {
-  const recipients = emailRecipients.split(',').map(email => email.trim())
+export async function sendEmail(data, recipients, subject) {
+  const finalRecipients =
+    recipients || emailRecipients.split(',').map(email => email.trim())
 
-  if (!recipients || recipients.length === 0)
+  if (!finalRecipients || finalRecipients.length === 0)
     return console.error('No hay destinatarios al que enviar el correo.')
 
   const { error } = await resend.emails.send({
     from: letyMindEmail,
-    to: recipients,
-    subject: 'Seguimiento semanal de uso de horas.',
+    to: finalRecipients,
+    subject: subject,
     html: data,
   })
 
